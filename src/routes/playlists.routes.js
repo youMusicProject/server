cb0a3830af36852e0887b8b7c94595a04701320e
Alvaro Alonso <airuritac@gmail.com>
2023-01-25 13:59:50 +0100

    rename routes

--- src/routes/playlists.routes.js
+++ src/routes/playlists.routes.js
@@ -5,16 +5,10 @@ const router = express.Router();
 const playlistController = require("../controller/playlists.controller")
 
 
-// Rutas de prueba
-router.get("/prueba", (req, res) => {
-    return res.json({
-        mensaje: "hola desde una prueba del back PLAYLIST"
-    })
-});
-
 router.get("/get", playlistController.getAllPlaylists)
-// VALIDAR SI ESTA LOGEADO CON UN USUARIO?
-router.post('/createplaylist', playlistController.createPlaylist)
+
+
+router.post('/new', playlistController.createPlaylist)
 
 
 module.exports = router;
